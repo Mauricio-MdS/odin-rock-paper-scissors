2d7aadb3f7ee525b7539482e4176fba75e60bb29
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-18 13:18:26 -0300

    feature: add a 5 round game

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ function capitalize(string){
     return string.charAt(0).toUpperCase().concat(string.substr(1))
 }
 
+function game(){
+    for (round = 1; round < 6; round++) {
+        let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+        playerSelection = capitalize(playerSelection);
+        while (isInvalidPlayerSelection(playerSelection)) {
+            alert("Invalid selection!")
+            playerSelection = prompt("Choose Rock, Paper, or Scissors");
+            playerSelection = capitalize(playerSelection);
+        }
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(`Round ${round}: ${roundResult}`)
+    }
+}
+
 function getComputerChoice() {
     const possibleChoices = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor(Math.random()*3)
     return possibleChoices[randomNumber];
+}
+
+function isInvalidPlayerSelection(playerSelection) {
+    return (playerSelection !== "Rock" && playerSelection !== "Paper" && playerSelection !== "Scissors")
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -22,7 +41,7 @@ function playRound(playerSelection, computerSelection) {
             return (computerSelection === "Rock") ? "You Win! Paper beats Rock" : "You Lose! Scissors beats Paper";
         case "Scissors":
             return (computerSelection === "Paper") ? "You Win! Scissors beats Paper" : "You Win! Paper beats Scissors";
-        default:
-            return "Invalid Selection"
     }
 }
+
+game();
