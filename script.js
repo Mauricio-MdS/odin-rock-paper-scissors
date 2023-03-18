e39979679b702bc1a4858f2006777a4539db03c9
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-18 13:05:16 -0300

    feat: add game round logic

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+function capitalize(string){
+    string = string.toLowerCase();
+    return string.charAt(0).toUpperCase().concat(string.substr(1))
+}
+
 function getComputerChoice() {
     const possibleChoices = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor(Math.random()*3)
     return possibleChoices[randomNumber];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+    if (playerSelection === computerSelection) {
+        return `It's a Tie! Both selected ${playerSelection}`
+    }
+
+    switch(playerSelection) {
+        case "Rock":
+            return (computerSelection === "Scissors") ? "You Win! Rock beats Scissors" : "You Lose! Paper beats Rock";
+        case "Paper":
+            return (computerSelection === "Rock") ? "You Win! Paper beats Rock" : "You Lose! Scissors beats Paper";
+        case "Scissors":
+            return (computerSelection === "Paper") ? "You Win! Scissors beats Paper" : "You Win! Paper beats Scissors";
+        default:
+            return "Invalid Selection"
+    }
 }
